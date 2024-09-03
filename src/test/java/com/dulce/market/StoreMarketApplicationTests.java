df739177879304a65c32054d4c0f35842e28e79b
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2024-09-03 13:20:02 -0600

    Configuración de application.properties para el desarrollo y produccion del proyecto

---
+++ src/test/java/com/dulce/market/StoreMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulce.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
