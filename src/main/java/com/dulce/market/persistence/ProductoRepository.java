2aa4d1d9be83f28f1e91fca4bc2bda278f2952ee
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2024-09-04 13:15:43 -0600

    Creación de Query Methods para Productos
    Se realizó una serie de Query para poder interactuar con la clase de Productos.
    - Lista de productos
    - Lista de productos por categoría
    - Lista de producto y precio
    - Recuperar los productos escasos

--- src/main/java/com/dulce/market/persistence/ProductoRepository.java
+++ src/main/java/com/dulce/market/persistence/ProductoRepository.java
@@ -4,11 +4,25 @@ import com.dulce.market.persistence.crud.ProductoCrudRepository;
 import com.dulce.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     //Creamos un metodo que recupere una lista de productos
-    public List<Producto> getAll(){
+    public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
     }
-}
+    //Muestra una lista de los productos por categoria
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+        }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    //Buscar producto por nombre y precio
+    public Optional<List<Producto>> getByProducto(String nombre, Integer precioVenta){
+        return Optional.ofNullable(productoCrudRepository.findByNombreAndPrecioVenta(nombre, precioVenta));
+    }
+    }
+
--- src/main/java/com/dulce/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/dulce/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,19 @@ package com.dulce.market.persistence.crud;
 import com.dulce.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 //Controlamos la tabla producto usando CrudRepository
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    //Recuperar toda la lista de productos que pertenezcan a una categoria en especifico
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true) --> Esta es una manera tradicional de manera nativa
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    //Queremos recuperar los productos escasos
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+    //Buscar producto por nombre y precio
+    List<Producto> findByNombreAndPrecioVenta(String nombre, Integer precioVenta);
 }
