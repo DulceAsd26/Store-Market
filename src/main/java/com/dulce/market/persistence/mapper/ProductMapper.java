ea237ba816c70987e24a6a259ffb7ab5d3be7a5f
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2024-09-04 22:22:06 -0600

    Orientando API con MapStruct
    Se realiza la orientación del repositorio a términos del dominio

---
+++ src/main/java/com/dulce/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,29 @@
+package com.dulce.market.persistence.mapper;
+
+import com.dulce.market.domain.Product;
+import com.dulce.market.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "precioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category")
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProducts(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+}
